497335d7e04b87dfb879f19a1e97a57d0fa6fb07
lisssq <lizynka05@yandex.ru>
2025-11-17 00:13:11 +0300

    fix tasts name

--- tasks/popova_e_integr_monte_carlo/tests/functional/main.cpp
+++ tasks/popova_e_integr_monte_carlo/tests/functional/main.cpp
@@ -33,20 +33,20 @@ class PopovaERunFuncTestsProcesses : public ppc::util::BaseRunFuncTests<InType, 
     int channels = -1;
     std::vector<uint8_t> img; 
     // Read image
-    // {
-    //   std::string abs_path = ppc::util::GetAbsoluteTaskPath(PPC_ID_popova_e_integr_monte_carlo, "pic.jpg");
-    //   auto *data = stbi_load(abs_path.c_str(), &width, &height, &channels, 0);
-    //   if (data == nullptr) {
-    //     throw std::runtime_error("Failed to load image: " + std::string(stbi_failure_reason()));
-    //   }
-    //   img = std::vector<uint8_t>(data, data + (static_cast<ptrdiff_t>(width * height * channels)));
-    //   stbi_image_free(data);
+    {
+      std::string abs_path = ppc::util::GetAbsoluteTaskPath(PPC_ID_popova_e_integr_monte_carlo, "pic.jpg");
+      auto *data = stbi_load(abs_path.c_str(), &width, &height, &channels, 0);
+      if (data == nullptr) {
+        throw std::runtime_error("Failed to load image: " + std::string(stbi_failure_reason()));
+      }
+      img = std::vector<uint8_t>(data, data + (static_cast<ptrdiff_t>(width * height * channels)));
+      stbi_image_free(data);
       
-    //   //if (std::cmp_not_equal(width, height)) {
-    //   if (width != height) {
-    //     throw std::runtime_error("width != height: ");
-    //   }
-    // }
+      //if (std::cmp_not_equal(width, height)) {
+      if (width != height) {
+        throw std::runtime_error("width != height: ");
+      }
+    }
 
     TestType params = std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kTestParams)>(GetParam());
     input_data_ = width - height + std::min(std::accumulate(img.begin(), img.end(), 0), channels);
